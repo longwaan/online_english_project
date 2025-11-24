92ef416ec43f7769173500e216e58ca5b07c8c1b
longwaan <1810193170@qq.com>
2025-11-24 10:15:58 +0800

    refactor:Element国际化

--- online_english_system-master/front_admin/src/main.ts
+++ online_english_system-master/front_admin/src/main.ts
@@ -6,6 +6,7 @@ import App from './App.vue'
 import router from './router'
 import './router/permission'  // 导入路由权限控制
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 import {createPinia} from "pinia";
 // import './style.css'
@@ -15,5 +16,7 @@ const app = createApp(App)
 pinia.use(piniaPersist);
 app.use(pinia)
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.mount('#app')
